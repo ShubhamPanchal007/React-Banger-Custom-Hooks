9de166c6bdd376eeefb90a6fb304c0b15041979c
Shubham Panchal <panchalshubham826@gmail.com>
2022-10-29 22:17:36 +0530

    Adding useMultistepForm hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+// // import { useEffect, useState } from "react";
+// import React, { useState } from "react";
+// import TestComp from "./Components/TestComp";
+// import { createContext } from "react";
 
+// export const moodContext = createContext();
+// export default function App() {
+//   // useState hook
+//   const [mood, setMood] = useState(false);
+
+//   // useEffect() hook
+
+//   // console.log("re-render triggerred! because of state change of counter");
+//   // let [counter, setCounter] = useState(0);
+//   // useEffect without any dependency --> componentDidMount() and componentDidUpdate()
+//   // And
+//   // If we pass an empty array as the second argument, it tells useEffect to nly fire on component load.
+//   // useEffect(() => {
+//   //   console.log(counter);
+//   //   return () => console.log("component is destroyed!!");
+//   // });
+//   // console.log(counter);
+
+//   // UseContext hook
+//   function moodChanger() {
+//     setMood(!mood);
+//   }
+
+//   return (
+//     <div>
+//       {/* {counter}
+//       {/* {setCounter(counter+1)} */}
+//       {/* <button onClick={() => setCounter(++counter)}>click</button> */}
+//       {/* <TestComp /> */}
+
+//       <button onClick={() => moodChanger()}>click</button>
+
+//       <moodContext.Provider value={mood}>
+//         <TestComp />
+//       </moodContext.Provider>
+//     </div>
+//   );
+// }
+
+// useRef hook
+
+import React from "react";
+import { useRef, useState, useEffect } from "react";
 function App() {
+  const [x, Setx] = useState(0);
+  const renderCount = useRef(0);
+
+  useEffect(() => {
+      // Every time the component re-renders update the renderCount;
+    });
+    renderCount.current = renderCount.current + 1;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {x}
+      <button onClick={() => Setx((prevx) => prevx + 1)}>Click</button>
+      <div>The app is re-rendered {renderCount.current} times</div>
     </div>
   );
 }
---
+++ src/Customhooks/useMultistepForm.tsx
@@ -0,0 +1,31 @@
+import { ReactElement, useState } from "react";
+
+export function useMultistepForm(steps: ReactElement[]) {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+  function next() {
+    setCurrentStepIndex((prevIndex) => {
+      if (steps.length - 1 <= currentStepIndex) return prevIndex;
+      return prevIndex + 1;
+    });
+  }
+  function back() {
+    setCurrentStepIndex((prevIndex) => {
+      if (prevIndex <= 0) return prevIndex;
+      return prevIndex - 1;
+    });
+  }
+  function goTo(index: number) {
+    setCurrentStepIndex(index);
+  }
+  return {
+    currentStepIndex,
+    step: steps[currentStepIndex],
+    steps,
+    isFirstStep: currentStepIndex === 0,
+    isLastStep: currentStepIndex === steps.length - 1,
+    goTo,
+    next,
+    back,
+  };
+}
